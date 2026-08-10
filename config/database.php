73dcc82712cdab1d8183d186f223b22945da89e3
daniel <daniel@vonrufs.com+>
2026-08-10 18:14:52 +0200

    change

---
+++ config/database.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'host' => 'localhost',
+    'database' => 'silberf1_saas',
+    'username' => 'silberf1_saas',
+    'password' => 'b7B0tMFG8*gmyElc',
+    'charset' => 'utf8mb4',
+];
